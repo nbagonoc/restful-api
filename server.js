a394dfb42719d8b5d588b2bcbac5a00bc4be794e
NiccuBagonoc <niccu.bagonoc@jpj.com>
2019-09-20 09:38:26 +0800

    backup before mac format

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+/////////////////////////////////////////
+// TODO - convert to into pack. directory
+/////////////////////////////////////////
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
