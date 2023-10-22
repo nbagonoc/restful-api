95a07b61e7bf5c60350b5f7dcf96a879e02c371e
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2023-10-22 16:11:28 +0800

    clean-up

--- test/getItem.test.js
+++ test/getItem.test.js
@@ -3,37 +3,32 @@ const { getItem } = require("../controllers/Item.controller");
 
 describe("Item Controller", () => {
     describe("getItem", () => {
-        afterEach(() => {
-            jest.restoreAllMocks();
-        });
+        const res = {
+            json: jest.fn(),
+            status: jest.fn().mockReturnThis(),
+        };
+        const req = { params: { id: "6533623125aced7f2d2d827c" } };
 
         it("should return the item if it exists", async () => {
             // Arrange
-            const item = {
+            const mockItem = {
                 _id: "6533623125aced7f2d2d827c",
                 name: "Test Item",
                 weight: 10,
                 size: "large",
             };
-            const req = {
-                params: {
-                    id: "6533623125aced7f2d2d827c"
-                }
-            };
-            const res = { json: jest.fn() };
-            jest.spyOn(Item, "findById").mockReturnValue({select: jest.fn().mockReturnValue(item)});
+            jest.spyOn(Item, "findById").mockReturnValue({
+                select: jest.fn().mockReturnValue(mockItem),
+            });
             // Act
             await getItem(req, res);
             // Assert
-            expect(res.json).toHaveBeenCalledWith(item);
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.json).toHaveBeenCalledWith(mockItem);
         });
 
         it("should return an error if the item does not exist", async () => {
             // Arrange
-            const req = { params: { id: "6533623125aced7f2d2d827c" } };
-            const res = {
-                status: jest.fn().mockReturnValue({ json: jest.fn() }),
-            };
             jest.spyOn(Item, "findById").mockReturnValue({
                 select: jest.fn().mockReturnValue(null),
             });
@@ -43,17 +38,13 @@ describe("Item Controller", () => {
             expect(res.status).toHaveBeenCalledWith(400);
             expect(res.status().json).toHaveBeenCalledWith({
                 errors: {
-                    message: "item not found"
+                    message: "item not found",
                 },
             });
         });
 
         it("should return a server error if an exception is thrown", async () => {
             //Arrange
-            const req = { params: { id: "6533623125aced7f2d2d827c" } };
-            const res = {
-                status: jest.fn().mockReturnValue({ json: jest.fn() }),
-            };
             jest.spyOn(Item, "findById").mockImplementation(() => {
                 throw new Error("Test Error");
             });
@@ -63,7 +54,7 @@ describe("Item Controller", () => {
             expect(res.status).toHaveBeenCalledWith(500);
             expect(res.status().json).toHaveBeenCalledWith({
                 errors: {
-                    message: "Something went wrong: Error: Test Error"
+                    message: "Something went wrong: Error: Test Error",
                 },
             });
         });
